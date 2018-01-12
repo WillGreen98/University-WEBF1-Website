ab6a59d0593dfeef1d6804423d9f1af38524918c
Will <will.green98@hotmail.com>
2018-01-12 15:12:26 +0000

    JS update classname

--- js/navigation.js
+++ js/navigation.js
@@ -1,53 +1,59 @@
-// var file_linkers = ["index.html", "discovery_log.html"];
-// var file_linker_headings = ["Home", "Discovery Log"];
-//
-// function createLiA() {
-//     var nav_li = document.createElement("li");
-//     nav_li.classList.add("nav_li");
-//
-//     var test = document.createTextNode(file_linker_headings);
-//
-//     for(i=0; i<=file_linkers.length; i++) {
-//         var newNav_a = document.createElement("a");
-//         newNav_a.classList.add("newNav_a");
-//         newNav_a.appendChild(test);
-//         newNav_a.setAttribute("href", file_linkers[i]);
-//         nav_li.appendChild(newNav_a);
-//         i++;
-//     }
-//     return nav_li;
-// }
-//
-// function loadNavigation() {
-//     var navigation = document.createElement("nav");
-//     navigation.classList.add("navigation");
-//
-//     var nav_ul = document.createElement("ul");
-//     nav_ul.classList.add("nav_ul");
-//
-//     for(var i=1; i<=file_linkers.length; i++) {
-//         nav_liA = createLiA();
-//         nav_ul.appendChild(nav_liA);
-//     }
-//
-//     navigation.appendChild(nav_ul);
-//
-//     var currentNav = document.getElementsByClassName("navigation")[0];
-//     document.body.insertBefore(navigation, currentNav);
-// }
-//
-// document.body.onload = loadNavigation;
+DEBUG = true;
 
-function loadNavigation() {
-    var nav = document.getElementById('navigation');
-    nav.insertAdjacentHTML('afterbegin',`
-    <div class="sidenav">
-      <a href="index.html">Home</a>
-      <a href="litreview.html">Litreview</a>
-      <a href="worklog.html">Worklogs</a>
-      <a href="video.html">Video</a>
-    </div>`
-  );
+if(DEBUG) {
+    var file_linkers = ["index.html", "discovery_log.html", "literature_review.html"];
+    var file_linker_headings = ["Home", "Discovery Log", "Literature Review"];21§§
+
+    function createLiA() {
+        var nav_li = document.createElement("li");
+        nav_li.classList.add("nav_li");
+
+        var heading = document.createTextNode(file_linker_headings);
+
+        for(i=0; i<=file_linkers.length; i++) {
+            var newNav_a = document.createElement("a");
+            newNav_a.classList.add("newNav_a");
+            newNav_a.appendChild(heading);
+            newNav_a.setAttribute("href", file_linkers[i]);
+            nav_li.appendChild(newNav_a);
+            i++;
+        }
+        return nav_li;
+    }
+
+    function loadNavigation() {
+        // creat and set class name on one line?
+        var navigation = document.createElement("nav");
+        navigation.classList.add("navigation");
+
+        var nav_ul = document.createElement("ul");
+        nav_ul.classList.add("nav_ul");
+
+        for(var i=1; i<=file_linkers.length; i++) {
+            nav_liA = createLiA();
+            nav_ul.appendChild(nav_liA);
+        }
+
+        navigation.appendChild(nav_ul);
+
+        var currentNav = document.getElementsByClassName("navigation")[0];
+        document.body.insertBefore(navigation, currentNav);
+    }
+
+    document.body.onload = loadNavigation;
+} else {
+    function loadNavigation() {
+        var nav = document.getElementById('navigation');
+        nav.insertAdjacentHTML('afterbegin',`
+        <nav class="navigation">
+            <ul class="nav_ul">
+                <li class="nav_li"><a href="index.html" class="nav_a">Home</a></li>
+                <li class="nav_li"><a href="./html/literature_review.html" class="nav_a">Literature Review</a></li>
+                <li class="nav_li"><a href="./html/discovery_log.html" class="nav_a">Discovery Log</a></li>
+            </ul>
+        </nav>`
+      );
+    }
+
+    loadNavigation();
 }
-
-loadNavigation();
