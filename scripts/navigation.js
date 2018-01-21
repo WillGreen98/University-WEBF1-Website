a32ab41fa24150ce2acf220c4231bb9932b1336a
Will <will.green98@hotmail.com>
2018-01-21 00:49:44 +0000

    DEBUG = false

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,54 +1,54 @@
-DEBUG = false;
+var DEBUG = false;
 
 if(DEBUG) {
-    let file_linkers = ["index.html", "uni.html" ,"discovery_log.html", "literature_review.html"];
-    let file_linker_headings = ["Home", "Uni" ,"Discovery Log", "Literature Review"];
+	let file_linkers = ["index.html", "uni.html" ,"discovery_log.html", "literature_review.html"];
+	let file_linker_headings = ["Home", "Uni" ,"Discovery Log", "Literature Review"];
 
-    function createLiA() {
-        let nav_li = document.createElement("li");
-        nav_li.classList.add("nav_li");
+	function createLiA() {
+		let nav_li = document.createElement("li");
+		nav_li.classList.add("nav_li");
+		
+		for(let i=0; i < file_linkers.length; i++) {
+			let newNav_a = document.createElement("a");
+			newNav_a.classList.add("newNav_a");
+			newNav_a.textContent = file_linker_headings[i];
+			newNav_a.setAttribute("href", file_linkers[i]);
+			nav_li.appendChild(newNav_a);
+		}
+		return nav_li;
+	}
 
-        for(let i=0; i < file_linkers.length; i++) {
-            let newNav_a = document.createElement("a");
-            newNav_a.classList.add("newNav_a");
-            newNav_a.textContent = file_linker_headings[i];
-            newNav_a.setAttribute("href", file_linkers[i]);
-            nav_li.appendChild(newNav_a);
-        }
-        return nav_li;
-    }
+	function loadNavigation() {
+		let navigation = document.createElement("nav");
+		navigation.classList.add("navigation");
 
-    function loadNavigation() {
-        let navigation = document.createElement("nav");
-        navigation.classList.add("navigation");
+		let nav_ul = document.createElement("ul");
+		nav_ul.classList.add("nav_ul");
+		
+		nav_liA = createLiA();
+		nav_ul.appendChild(nav_liA);
 
-        let nav_ul = document.createElement("ul");
-        nav_ul.classList.add("nav_ul");
-        
-        nav_liA = createLiA();
-        nav_ul.appendChild(nav_liA);
+		navigation.appendChild(nav_ul);
 
-        navigation.appendChild(nav_ul);
+		let currentNav = document.getElementsByClassName("navigation")[0];
+		document.body.insertBefore(navigation, currentNav);
+	}
 
-        let currentNav = document.getElementsByClassName("navigation")[0];
-        document.body.insertBefore(navigation, currentNav);
-    }
-
-    document.body.onload = loadNavigation;
+	document.body.onload = loadNavigation;
 } else {
-    function loadNavigation() {
-        let nav = document.getElementById('navigation');
-        nav.insertAdjacentHTML('afterbegin',`
-        <nav class="navigation">
-            <ul class="nav_ul">
-                <li class="nav_li"><a href="index.html" class="nav_a">Home</a></li>
-                <li class="nav_li"><a href="../views/uni.html" class="nav_a">Uni</a></li>
-                <li class="nav_li"><a href="../views/literature_review.html" class="nav_a">Literature Review</a></li>
-                <li class="nav_li"><a href="../views/discovery_log.html" class="nav_a">Discovery Log</a></li>
-            </ul>
-        </nav>`
-      );
-    }
+	function loadNavigation() {
+		let nav = document.getElementById('navigation');
+		nav.insertAdjacentHTML('afterbegin',`
+		<nav class="navigation">
+			<ul class="nav_ul">
+				<li class="nav_li"><a href="index.html" class="nav_a">Home</a></li>
+				<li class="nav_li"><a href="../views/uni.html" class="nav_a">Uni</a></li>
+				<li class="nav_li"><a href="../views/literature_review.html" class="nav_a">Literature Review</a></li>
+				<li class="nav_li"><a href="../views/discovery_log.html" class="nav_a">Discovery Log</a></li>
+			</ul>
+		</nav>`
+	  );
+	}
 
-    loadNavigation();
+	loadNavigation();
 }
