b28623ff9d30f2bd7a1402abd7659eea48f4c773
Will <will.green98@hotmail.com>
2018-01-22 10:11:57 +0000

    Test Nav -> JS

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,6 @@
-const DEBUG = false;
+const PRODUCTION = false;
 
-if(DEBUG) {
+if(PRODUCTION == true) {
 	let file_linkers = ["index.html", "uni.html" ,"discovery_log.html", "literature_review.html"];
 	let file_linker_headings = ["Home", "Uni" ,"Discovery Log", "Literature Review"];
 
@@ -40,13 +40,20 @@ if(DEBUG) {
 		let nav = document.getElementById("navigation");
 		nav.insertAdjacentHTML("afterbegin",`
 		<nav class="navigation">
-			<ul class="nav_ul">
-				<li class="nav_li"><a href="index.html" class="nav_a">Home</a></li>
-				<li class="nav_li"><a href="../views/uni.html" class="nav_a">Uni</a></li>
-				<li class="nav_li"><a href="../views/literature_review.html" class="nav_a">Literature Review</a></li>
-				<li class="nav_li"><a href="../views/discovery_log.html" class="nav_a">Discovery Log</a></li>
-			</ul>
-		</nav>`
+            <ul class="nav_ul">
+                <li class="nav_li"><a href="index.html" class="nav_a">Home</a></li>
+                <li class="nav_li"><a href="./views/literature_review.html" class="nav_a">Literature Review</a></li>
+                <li class="nav_li"><a href="./views/discovery_log.html" class="nav_a">Discovery Log</a></li>
+                <li class="nav_li"><a href="./views/uni.html" class="nav_a">Uni</a></li>
+                <li class="nav_li"><a href="./views/web-dev-standards.html" class="nav_a">Web Standards</a></li>
+                <li class="nav_li"><a href="./views/Modules/CAR.html" class="nav_a">CAR</a></li>
+                <li class="nav_li"><a href="./views/Modules/COMP-TUT.html" class="nav_a"></a></li>
+                <li class="nav_li"><a href="./views/Modules/INDADD.html" class="nav_a"></a></li>
+                <li class="nav_li"><a href="./views/Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
+                <li class="nav_li"><a href="./views/Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
+                <li class="nav_li"><a href="./views/Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
+            </ul>
+        </nav>`
 	  );
 	}
 
