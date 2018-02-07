897f87a0b21c1c62a8d45dd0edf3e31bfff830ec
Will <will.green98@hotmail.com>
2018-02-07 23:54:57 +0000

    Nav Updates

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,59 +1,5 @@
 const PRODUCTION = true;
 
-function page_Active() {
-	let link_list = [URL];
-	let link_Active = document.getElementById("navigation").getElementsByTagName("a");
-	link_list.appendChild(link_Active);
-
-	for(i=0; i<link_list.length; i++) { 
-		if(document.location.href.indexOf(link_Active[i].href)>=0) {
-			link_Active[i].className="active";
-	  	}
-	}
-}
-
-function loadNavigation_Index() {
-	let nav = document.getElementById("navigation");
-	nav.insertAdjacentHTML("afterbegin",`
-	<nav class="navigation">
-            <ul class="nav_ul">
-                <li class="nav_li"><a href="index.html" class="nav_a">Home</a></li>
-                <li class="nav_li"><a href="./views/literature_review.html" class="nav_a">Literature Review</a></li>
-                <li class="nav_li"><a href="./views/discovery_log.html" class="nav_a">Discovery Log</a></li>
-                <li class="nav_li"><a href="./views/uni.html" class="nav_a">Uni</a></li>
-                <li class="nav_li"><a href="./views/web-dev-standards.html" class="nav_a">Web Standards</a></li>
-                <li class="nav_li"><a href="./views/Modules/CAR.html" class="nav_a">CAR</a></li>
-                <li class="nav_li"><a href="./views/Modules/COMP-TUT.html" class="nav_a"></a></li>
-                <li class="nav_li"><a href="./views/Modules/INDADD.html" class="nav_a"></a></li>
-                <li class="nav_li"><a href="./views/Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
-                <li class="nav_li"><a href="./views/Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
-                <li class="nav_li"><a href="./views/Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
-            </ul>
-        </nav>`
-	);
-}
-
-function loadNavigation() {
-	let nav = document.getElementById("navigation");
-	nav.insertAdjacentHTML("afterbegin",`
-	<nav class="navigation">
-            <ul class="nav_ul">
-                <li class="nav_li"><a href="../index.html" class="nav_a">Home</a></li>
-                <li class="nav_li"><a href="./literature_review.html" class="nav_a">Literature Review</a></li>
-                <li class="nav_li"><a href="./discovery_log.html" class="nav_a">Discovery Log</a></li>
-                <li class="nav_li"><a href="./uni.html" class="nav_a">Uni</a></li>
-                <li class="nav_li"><a href="./web-dev-standards.html" class="nav_a">Web Standards</a></li>
-                <li class="nav_li"><a href="./Modules/CAR.html" class="nav_a">CAR</a></li>
-                <li class="nav_li"><a href="./Modules/COMP-TUT.html" class="nav_a"></a></li>
-                <li class="nav_li"><a href="./Modules/INDADD.html" class="nav_a"></a></li>
-                <li class="nav_li"><a href="./Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
-                <li class="nav_li"><a href="./Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
-                <li class="nav_li"><a href="./Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
-            </ul>
-        </nav>`
-	);
-}
-
 let file_linkers = ["index.html", "uni.html" ,"discovery_log.html", "literature_review.html"];
 let file_linker_headings = ["Home", "Uni" ,"Discovery Log", "Literature Review"];
 
@@ -87,13 +33,92 @@ function loadNavigation_DOM() {
 	document.body.insertBefore(navigation, currentNav);
 }
 
+function page_Active() {
+	let link_list = [];
+	let link_Active = document.getElementById("navigation").getElementsByTagName("a");
+	link_list.appendChild(link_Active);
+
+	for(let i=0; i<link_list.length; i++) { 
+		if(document.location.href.indexOf(link_Active[i].href)>=0) {
+			link_Active[i].className="active";
+		}
+	}
+}
+
+function loadNavigation_Index() {
+	let nav = document.getElementById("navigation");
+	nav.insertAdjacentHTML("afterbegin",`
+	<nav id="navigation">
+            <ul class="nav_ul">
+                <li class="nav_li"><a href="index.html" class="nav_a">Home</a></li>
+                <li class="nav_li"><a href="./views/literature_review.html" class="nav_a">Literature Review</a></li>
+                <li class="nav_li"><a href="./views/discovery_log.html" class="nav_a">Discovery Log</a></li>
+                <li class="nav_li"><a href="./views/uni.html" class="nav_a">Uni></a></li>
+                    <div class="dropdown_content">
+                        <ul class="navigation_modules">
+                            <li class="nav_li"><a href="./views/Modules/CAR.html" class="nav_a"><abbr title="Computer Architecture">CAR</abbr></a></li>
+                            <li class="nav_li"><a href="./views/Modules/COMP-TUT.html" class="nav_a"><abbr title="Computer Tutorial">COMP TUT</abbr></a></li>
+                            <li class="nav_li"><a href="./views/Modules/INDADD.html" class="nav_a"><abbr title="Introduction To Databases and Design">INDADD</abbr></a></li>
+                            <li class="nav_li"><a href="./views/Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
+                            <li class="nav_li"><a href="./views/Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
+                            <li class="nav_li"><a href="./views/Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
+                        </ul>
+                    </div>
+                <li class="nav_li"><a href="./views/web-dev-standards.html" class="nav_a">Web Standards</a></li>
+            </ul>
+        </nav>`
+	);
+}
+
+function loadNavigation_Views() {
+	let nav = document.getElementById("navigation");
+	nav.insertAdjacentHTML("afterbegin",`
+	<nav class="navigation">
+            <ul class="nav_ul">
+                <li class="nav_li"><a href="../index.html" class="nav_a">Home</a></li>
+                <li class="nav_li"><a href="./literature_review.html" class="nav_a">Literature Review</a></li>
+                <li class="nav_li"><a href="./discovery_log.html" class="nav_a">Discovery Log</a></li>
+                <li class="nav_li"><a href="./uni.html" class="nav_a">Uni</a></li>
+                <li class="nav_li"><a href="./web-dev-standards.html" class="nav_a">Web Standards</a></li>
+                <li class="nav_li"><a href="./Modules/CAR.html" class="nav_a">CAR</a></li>
+                <li class="nav_li"><a href="./Modules/COMP-TUT.html" class="nav_a"></a></li>
+                <li class="nav_li"><a href="./Modules/INDADD.html" class="nav_a"></a></li>
+                <li class="nav_li"><a href="./Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
+                <li class="nav_li"><a href="./Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
+                <li class="nav_li"><a href="./Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
+            </ul>
+        </nav>`
+	);
+}
+
+function loadNavigation_Modules() {
+	let nav = document.getElementById("navigation");
+	nav.insertAdjacentHTML("afterbegin",`
+	<nav class="navigation">
+            <ul class="nav_ul">
+                <li class="nav_li"><a href="../index.html" class="nav_a">Home</a></li>
+                <li class="nav_li"><a href="./views/literature_review.html" class="nav_a">Literature Review</a></li>
+                <li class="nav_li"><a href="./views/discovery_log.html" class="nav_a">Discovery Log</a></li>
+                <li class="nav_li"><a href="./views/uni.html" class="nav_a">Uni</a></li>
+                <li class="nav_li"><a href="./views/web-dev-standards.html" class="nav_a">Web Standards</a></li>
+                <li class="nav_li"><a href="./views/Modules/CAR.html" class="nav_a">CAR</a></li>
+                <li class="nav_li"><a href="./views/Modules/COMP-TUT.html" class="nav_a"></a></li>
+                <li class="nav_li"><a href="./views/Modules/INDADD.html" class="nav_a"></a></li>
+                <li class="nav_li"><a href="./views/Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
+                <li class="nav_li"><a href="./views/Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
+                <li class="nav_li"><a href="./views/Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
+            </ul>
+        </nav>`
+	);
+}
+
 let index_page_path = "../index.html";
 if(PRODUCTION == true) {
 	if(window.location.pathname == index_page_path) {
 		window.onload = page_Active();
 		loadNavigation_Index();
 	} else {
-		loadNavigation();
+		loadNavigation_Views();
 	}
 } else {
 	window.onload = page_Active();
