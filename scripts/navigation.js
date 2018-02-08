1060b84fe4ed31eeae2c182784d97e842ba57350
Will <will.green98@hotmail.com>
2018-02-08 13:12:48 +0000

    Updates

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -18,17 +18,17 @@ function loadNavigation_Index() {
 	<nav id="navigation">
             <ul class="nav_ul">
                 <li class="nav_li"><a href="index.html" class="nav_a">Home</a></li>
-                <li class="nav_li"><a href="./views/literature_review.html" class="nav_a">Literature Review</a></li>
-                <li class="nav_li"><a href="./views/discovery_log.html" class="nav_a">Discovery Log</a></li>
-                <li class="nav_li"><a href="./views/uni.html" class="nav_a">Uni></a></li>
+                <li class="nav_li"><a href="/views/literature_review.html" class="nav_a">Literature Review</a></li>
+                <li class="nav_li"><a href="/views/discovery_log.html" class="nav_a">Discovery Log</a></li>
+                <li class="nav_li"><a href="/views/uni.html" class="nav_a">Uni></a></li>
                     <div class="dropdown_content">
                         <ul class="navigation_modules">
-                            <li class="nav_li"><a href="./views/Modules/CAR.html" class="nav_a"><abbr title="Computer Architecture">CAR</abbr></a></li>
-                            <li class="nav_li"><a href="./views/Modules/COMP-TUT.html" class="nav_a"><abbr title="Computer Tutorial">COMP TUT</abbr></a></li>
-                            <li class="nav_li"><a href="./views/Modules/INDADD.html" class="nav_a"><abbr title="Introduction To Databases and Design">INDADD</abbr></a></li>
-                            <li class="nav_li"><a href="./views/Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
-                            <li class="nav_li"><a href="./views/Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
-                            <li class="nav_li"><a href="./views/Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
+                            <li class="nav_li"><a href="/views/Modules/CAR.html" class="nav_a"><abbr title="Computer Architecture">CAR</abbr></a></li>
+                            <li class="nav_li"><a href="/views/Modules/COMP-TUT.html" class="nav_a"><abbr title="Computer Tutorial">COMP TUT</abbr></a></li>
+                            <li class="nav_li"><a href="/views/Modules/INDADD.html" class="nav_a"><abbr title="Introduction To Databases and Design">INDADD</abbr></a></li>
+                            <li class="nav_li"><a href="/views/Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
+                            <li class="nav_li"><a href="/views/Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
+                            <li class="nav_li"><a href="/views/Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
                         </ul>
                     </div>
                 <li class="nav_li"><a href="./views/web-dev-standards.html" class="nav_a">Web Standards</a></li>
@@ -42,14 +42,14 @@ function loadNavigation_Views() {
 	nav.insertAdjacentHTML("afterbegin",`
 	<nav class="navigation">
             <ul class="nav_ul">
-                <li class="nav_li"><a href="../index.html" class="nav_a">Home</a></li>
-                <li class="nav_li"><a href="./views/literature_review.html" class="nav_a">Literature Review</a></li>
-                <li class="nav_li"><a href="./views/discovery_log.html" class="nav_a">Discovery Log</a></li>
-                <li class="nav_li"><a href="./views/uni.html" class="nav_a">Uni</a></li>
-                <li class="nav_li"><a href="./views/web-dev-standards.html" class="nav_a">Web Standards</a></li>
+                <li class="nav_li"><a href="/index.html" class="nav_a">Home</a></li>  
+                <li class="nav_li"><a href="literature_review.html" class="nav_a">Literature Review</a></li>
+                <li class="nav_li"><a href="discovery_log.html" class="nav_a">Discovery Log</a></li>
+                <li class="nav_li"><a href="uni.html" class="nav_a">Uni</a></li>
+                <li class="nav_li"><a href="web-dev-standards.html" class="nav_a">Web Standards</a></li>
                 <li class="nav_li"><a href="./views/Modules/CAR.html" class="nav_a">CAR</a></li>
                 <li class="nav_li"><a href="./views/Modules/COMP-TUT.html" class="nav_a">Comp Tut</a></li>
-                <li class="nav_li"><a href="./views/Modules/INDADD.html" class="nav_a">INDDAD</a></li>
+                <li class="nav_li"><a href="./views/Modules/INDADD.html" class="nav_a">INDADD</a></li>
                 <li class="nav_li"><a href="./views/Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
                 <li class="nav_li"><a href="./views/Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
                 <li class="nav_li"><a href="./views/Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
@@ -63,36 +63,59 @@ function loadNavigation_Modules() {
 	nav.insertAdjacentHTML("afterbegin",`
 	<nav class="navigation">
             <ul class="nav_ul">
-                <li class="nav_li"><a href="../index.html" class="nav_a">Home</a></li>
-                <li class="nav_li"><a href="./views/literature_review.html" class="nav_a">Literature Review</a></li>
-                <li class="nav_li"><a href="./views/discovery_log.html" class="nav_a">Discovery Log</a></li>
-                <li class="nav_li"><a href="./views/uni.html" class="nav_a">Uni</a></li>
-                <li class="nav_li"><a href="./views/web-dev-standards.html" class="nav_a">Web Standards</a></li>
-                <li class="nav_li"><a href="./views/Modules/CAR.html" class="nav_a">CAR</a></li>
-                <li class="nav_li"><a href="./views/Modules/COMP-TUT.html" class="nav_a">Comp Tut</a></li>
-                <li class="nav_li"><a href="./views/Modules/INDADD.html" class="nav_a">INDDAD</a></li>
-                <li class="nav_li"><a href="./views/Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
-                <li class="nav_li"><a href="./views/Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
-                <li class="nav_li"><a href="./views/Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
+                <li class="nav_li"><a href="/index.html" class="nav_a">Home</a></li>
+                <li class="nav_li"><a href="/views/literature_review.html" class="nav_a">Literature Review</a></li>
+                <li class="nav_li"><a href="/views/discovery_log.html" class="nav_a">Discovery Log</a></li>
+                <li class="nav_li"><a href="/views/uni.html" class="nav_a">Uni</a></li>
+                <li class="nav_li"><a href="/views/web-dev-standards.html" class="nav_a">Web Standards</a></li>
+                <li class="nav_li"><a href="CAR.html" class="nav_a">CAR</a></li>
+                <li class="nav_li"><a href="COMP-TUT.html" class="nav_a">Comp Tut</a></li>
+                <li class="nav_li"><a href="INDADD.html" class="nav_a">INDADD</a></li>
+                <li class="nav_li"><a href="INTPROG.html" class="nav_a">INTPROG</a></li>
+                <li class="nav_li"><a href="NETFUN.html" class="nav_a">NETFUN</a></li>
+                <li class="nav_li"><a href="WEBF1.html" class="nav_a">WEBF1</a></li>
             </ul>
         </nav>`
 	);
 }
 
-let loc = window.location.pathname;
-let dir = loc.substring(0, loc.lastIndexOf("/"));
+let url = window.location.pathname;
+let directory = url.substring(0, url.lastIndexOf("/"));
+let file = url.substring(url.lastIndexOf("/")+1);
 
 if(PRODUCTION == true) {
-	page_Active();
-	switch(dir) {
-	case "/":
+	switch(file) {
+	case "index.html": 
 		loadNavigation_Index();
 		break;
-	case "/views":
+	case "discovery.html":
+	case "literature_review.html":
+	case "uni.html":
+	case "web-dev-standards.html":
 		loadNavigation_Views();
 		break;
-	case "/views/Modules/":
+	case "CAR.html":
+	case "COMP-TUT.html":
+	case "INDADD.html":
+	case "INTPROG.html":
+	case "NETFUN.html":
+	case "WEBF1.html":
 		loadNavigation_Modules();
 		break;
 	}
 }
+
+// if(PRODUCTION == true) {
+// 	page_Active();
+// 	switch(directory) {
+// 	case "/":
+// 		loadNavigation_Index();
+// 		break;
+// 	case "/views":
+// 		loadNavigation_Views();
+// 		break;
+// 	case "/views/Modules/":
+// 		loadNavigation_Modules();
+// 		break;
+// 	}
+// }
