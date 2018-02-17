d9e7e1b21d108738ce1c74801b73b9e8a6dc6d4e
Will <will.green98@hotmail.com>
2018-02-17 13:47:50 +0000

    Update

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -18,20 +18,20 @@ function loadNavigation_Index() {
 	<nav id="navigation">
             <ul class="nav_ul">
                 <li class="nav_li"><a href="index.html" class="nav_a">Home</a></li>
-                <li class="nav_li"><a href="./views/literature_review.html" class="nav_a">Literature Review</a></li>
-                <li class="nav_li"><a href="./views/discovery_log.html" class="nav_a">Discovery Log</a></li>
-                <li class="nav_li"><a href="./views/uni.html" class="nav_a">Uni></a></li>
+                <li class="nav_li"><a href="/views/literature_review.html" class="nav_a">Literature Review</a></li>
+                <li class="nav_li"><a href="/views/discovery_log.html" class="nav_a">Discovery Log</a></li>
+                <li class="nav_li"><a href="/views/uni.html" class="nav_a">Uni></a></li>
                     <div class="dropdown_content">
                         <ul class="navigation_modules">
-                            <li class="nav_li"><a href="./views/Modules/CAR.html" class="nav_a"><abbr title="Computer Architecture">CAR</abbr></a></li>
-                            <li class="nav_li"><a href="./views/Modules/COMP-TUT.html" class="nav_a"><abbr title="Computer Tutorial">COMP TUT</abbr></a></li>
-                            <li class="nav_li"><a href="./views/Modules/INDADD.html" class="nav_a"><abbr title="Introduction To Databases and Design">INDADD</abbr></a></li>
-                            <li class="nav_li"><a href="./views/Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
-                            <li class="nav_li"><a href="./views/Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
-                            <li class="nav_li"><a href="./views/Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
+                            <li class="nav_li"><a href="/views/Modules/CAR.html" class="nav_a"><abbr title="Computer Architecture">CAR</abbr></a></li>
+                            <li class="nav_li"><a href="/views/Modules/COMP-TUT.html" class="nav_a"><abbr title="Computer Tutorial">COMP TUT</abbr></a></li>
+                            <li class="nav_li"><a href="/views/Modules/INDADD.html" class="nav_a"><abbr title="Introduction To Databases and Design">INDADD</abbr></a></li>
+                            <li class="nav_li"><a href="/views/Modules/INTPROG.html" class="nav_a">INTPROG</a></li>
+                            <li class="nav_li"><a href="/views/Modules/NETFUN.html" class="nav_a">NETFUN</a></li>
+                            <li class="nav_li"><a href="/views/Modules/WEBF1.html" class="nav_a">WEBF1</a></li>
                         </ul>
                     </div>
-                <li class="nav_li"><a href="./views/web-dev-standards.html" class="nav_a">Web Standards</a></li>
+                <li class="nav_li"><a href="/views/web-dev-standards.html" class="nav_a">Web Standards</a></li>
             </ul>
         </nav>`
 	);
