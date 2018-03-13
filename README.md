ab73f28cf5e3a53071941efdccf91a1c42d383a6
Will <will.green98@hotmail.com>
2018-03-13 17:12:18 +0000

    Adventure Init Upload

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # University-WEBF1-Website
 
 These pages are apart of my WEBF1 website coursework, it will contain:
-
 1. Index.html
 2. Views -> HTML
 3. Styling -> CSS
@@ -9,8 +8,10 @@ These pages are apart of my WEBF1 website coursework, it will contain:
 5. Error-Pages -> 404 and a small joke.
 6. Downloads -> Files for downloads
 7. Robots.txt
+8. A penis
 
 I have been given a lengthy three page document of web standards that I will have to meet as
 they will be required for me to gain or not lose marks.
+
 
 A viewing of my site will be available when clicking -> [Link to site](https://willgreen98.github.io/University-WEBF1-Website)
--- scripts/footer.js
+++ scripts/footer.js
@@ -1,29 +1,5 @@
-function loadNavigation_Index() {
-	let footer = document.getElementById("footer").insertAdjacentHTML('afterbegin',`
-    <footer id="footer_container" role="footer">
-        <header id="footer_header">
-            <h3 id="footer_h">Site Footer</h3>
-        </header>
+document.body.onload = loadFooter;
 
-        <div class="img_container">
-            <a href="https://validator.w3.org/check?uri=referer" target="_blank">
-                <picture class="w3_href_picture">
-                    <source srcset=""https://www.w3.org/Icons/valid-html401-blue"">
-                    <img src="https://www.w3.org/Icons/valid-html401-blue" alt="Validate HTML">
-                </picture>
-            </a>
-        </div>
+function loadFooter() {
 
-        <div class="img_container">
-            <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">
-                <picture class="w3_href_picture">
-                    <source srcset="http://jigsaw.w3.org/css-validator/images/vcss">
-                    <img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Validate CSS">
-                </picture>
-            </a>
-        </div>
-    </footer>
-    `);
 }
-
-window.onload = loadNavigation_Index();
